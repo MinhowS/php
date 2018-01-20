55613138c4657ca213427b56c0aeb8bc6744be46
MinhowS <lxdiudong@hotmail.com>
2018-01-20 23:04:21 +0800

    abc是基础

---
+++ base/1.abc.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 2018/1/20
+ * Time: 22:57
+ */
+/*phpinfo是一个函数（功能），这个函数（功能）会显示一个当前电脑（服务器）的详细的PHP信息*/
+phpinfo();
---
+++ base/4.zifuchuanbianliang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 2018/1/19
+ * Time: 14:46
+ */
+$txt1 = 'Hello world!';
+$txt2 = 'What a nice day！';
+echo $txt1.' '.$txt2;
+echo '<br>';
+echo 'strlen()函数返回字符串的长度';
+echo strlen($txt1);
+echo '<br>';
+echo 'strpos()函数返回指定字符或者字符串出现的位置，没有返回false';
+echo strpos('hello world','world');

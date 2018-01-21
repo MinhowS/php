4d348aa21fa7de371795856b96e9492c5d83aeba
MinhowS <lxdliudong@hotmail.com>
2018-01-21 11:00:48 +0800

    echo

---
+++ base/echo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 2018/1/21
+ * Time: 10:44
+ */
+echo '注释';
+//声明一部iphone7 手机的价格
+$iphone7_price = 6188;
+
+//显示输出手机的价格
+echo $iphone7_price;

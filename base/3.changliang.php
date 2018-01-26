98efb64ba6ea73075fb3c198c5704f0a88868b8b
MinhowS <lxdliudong@hotmail.com>
2018-01-26 19:47:07 +0800

    常量补充

--- base/3.changliang.php
+++ base/3.changliang.php
@@ -5,10 +5,11 @@
  * Date: 2018/1/19
  * Time: 14:23
  */
+header('Content-type: text/html; charset=utf-8');
 echo '定义一个常量用define()函数';
 echo '<br>';
-define("GREET","这是php的常量",true);
+define("GREET","这是php的常量",true);//true表示对大小写不敏感
 function test(){
     echo Greet;//定义的变量在函数内也可以正常访问。
 }
-test();
+test();//这是php的常量

f83a4bf3b0e52f999d483096680ed4fcc816844c
MinhowS <lxdliudong@hotmail.com>
2018-03-15 17:22:26 +0800

    更新

---
+++ base/hanshu/test.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-type: text/html; charset=utf-8');/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 2018/3/15
+ * Time: 17:18
+ */
+echo '测试一下注释<?php    ?>会不会产生什么影响';
+echo '<br />';
+
+?>
+这是什么？<?php /*if (true) {
+    echo 'good';
+}else {
+    echo 'bad';
+} */?>答案
+<?php
+echo '<br />';
+echo '如果注释<?php    ?>不会产生影响，相当于不会解析';
+?>
